b84b942aac984e7289da402dea40ad4acced984a
Jose Tom <jose@chargebee.com>
2023-03-08 11:27:06 +0530

    chore: working local setup using pricify js

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,29 +1,57 @@
-import type { DimensionEvent } from "@/events/defn";
-import { EventKeys } from "@/events/defn";
-import useMessage from "@/events/listener";
-import { useState } from "react";
+import Pricify from "@atomicpricing/pricify";
+import { useEffect } from "react";
+
+// height and width
+// if provided, then frame is bounded
+// if not, then it autoscales
 
 export default function PricingPage() {
-  const [dimension, setDimension] = useState<DimensionEvent>({});
-
-  useMessage(
-    EventKeys.DIMENSION,
-    (data) => {
-      console.log(data);
-      setDimension(data);
-    },
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    new URL(process.env.NEXT_PUBLIC_ATOMIC_PRICING_HOSTED_PAGE!).origin
-  );
+  useEffect(() => {
+    Pricify.init();
+  }, []);
 
   return (
-    <iframe
-      style={{
-        height: dimension.scroll?.height,
-        // width: dimension.scroll?.width,
-      }}
-      src={process.env.NEXT_PUBLIC_ATOMIC_PRICING_HOSTED_PAGE}
-      className="w-full bg-inherit"
-    />
+    <>
+      <div
+        id="pricify-hosted-pricing-page"
+        data-pricify-site="01GTKTPXN46E55JG69QEHV2BXC"
+        data-pricify-pricingpage="default"
+      ></div>
+      <div>
+        <p className="mt-24 text-center text-xs font-bold uppercase tracking-[0.3em] text-zinc-400">
+          Brought to you by
+        </p>
+        <div className="my-12 flex flex-col items-center space-y-4 sm:mt-8 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 md:mx-auto md:max-w-2xl">
+          <div className="flex items-center justify-center">
+            <a
+              target="_blank"
+              href="https://chargebee.com"
+              rel="noreferrer"
+              aria-label="chargebee.com Link"
+            >
+              <img
+                src="/chargebee.svg"
+                alt="chargebee.com Logo"
+                className="h-14 text-white"
+              />
+            </a>
+          </div>
+          <div className="flex items-center justify-center">
+            <a
+              target="_blank"
+              href="https://atomicpricing.com"
+              rel="noreferrer"
+              aria-label="atomicpricing.com Link"
+            >
+              <img
+                src="/atomicpricing-white.svg"
+                alt="atomicpricing.com Logo"
+                className="h-14 text-white"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
